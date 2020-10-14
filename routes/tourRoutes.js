438eb1b0e684b97e371d183a6b9ea76ba3502742
Mohammad Barati <mohammad.barati@outlook.com>
2020-10-14 16:08:26 +0330

    Implement Nested Route for Reviews based on Tour

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 //router.param('id', tourController.checkID);
+
+// implement Nested Routes
+
+// POST/tour/jgfakg63126/reviews
+// GET/tour/jgfakg63126/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getToursStatistics);
